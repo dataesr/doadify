987ea81124127c6ce16ccb8cde9672b17b29bd6c
folland87 <frederic@olland.me>
2023-05-12 17:38:08 +0200

    client and fullstack

--- index.js
+++ index.js
@@ -1,103 +1,121 @@
 #!/usr/bin/env node
 
 // Usage: npx @dataesr/doadify my-app
-import spawn from 'cross-spawn';
 import fs from 'fs';
 import path from 'path';
-// import inquirer from 'inquirer';
-// import chalk from 'chalk';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
 import { fileURLToPath } from 'url';
 
-// The first argument will be the project name.
+async function scaffoldServer(projectName) {
+  const currentDir = process.cwd();
+  const projectDir = path.resolve(currentDir, projectName);
+  fs.mkdirSync(projectDir, { recursive: true });
+  
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const templateDir = path.resolve(__dirname, 'templates/fullstack');
+  fs.cpSync(templateDir, projectDir, { recursive: true });
+  
+  // Update the project's package.json with the new project name
+  const { default: projectPackageJson } = await import(
+    `${path.join(projectDir, 'package.json')}`,
+    { assert: { type: 'json' } }
+  );
+  projectPackageJson.name = projectName;
+  fs.writeFileSync(
+    path.join(projectDir, 'package.json'),
+    JSON.stringify(projectPackageJson, null, 2)
+  );
+
+  // rename the dotfiles after we have copied them over to the new project directory.
+  fs.renameSync(path.join(projectDir, '_github'), path.join(projectDir, '.github'));
+  const clientDir = path.resolve(projectDir, 'client');
+  fs.renameSync(path.join(clientDir, '_env'), path.join(clientDir, '.env'));
+  fs.renameSync(path.join(clientDir, '_env.staging'), path.join(clientDir, '.env.staging'));
+  fs.renameSync(path.join(clientDir, '_env.production'), path.join(clientDir, '.env.production'));
+  fs.renameSync(path.join(clientDir, '_eslintrc'), path.join(clientDir, '.eslintrc'));
+  fs.renameSync(path.join(clientDir, '_gitignore'), path.join(clientDir, '.gitignore'));
+  const serverDir = path.resolve(projectDir, 'server');
+  fs.renameSync(path.join(serverDir, '_eslintrc'), path.join(serverDir, '.eslintrc'));
+  fs.renameSync(path.join(serverDir, '_gitignore'), path.join(serverDir, '.gitignore'));
+  
+}
+
+async function scaffoldClient(projectName) {
+  const currentDir = process.cwd();
+  const projectDir = path.resolve(currentDir, projectName);
+  fs.mkdirSync(projectDir, { recursive: true });
+  
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const templateDir = path.resolve(__dirname, 'templates/client');
+  fs.cpSync(templateDir, projectDir, { recursive: true });
+  
+  // rename the dotfiles after we have copied them over to the new project directory.
+  fs.renameSync(path.join(projectDir, '_github'), path.join(projectDir, '.github'));
+  fs.renameSync(path.join(projectDir, '_env'), path.join(projectDir, '.env'));
+  fs.renameSync(path.join(projectDir, '_env.staging'), path.join(projectDir, '.env.staging'));
+  fs.renameSync(path.join(projectDir, '_env.production'), path.join(projectDir, '.env.production'));
+  fs.renameSync(path.join(projectDir, '_eslintrc'), path.join(projectDir, '.eslintrc'));
+  fs.renameSync(path.join(projectDir, '_gitignore'), path.join(projectDir, '.gitignore'));
+  
+  // Update the project's package.json with the new project name
+  const { default: projectPackageJson } = await import(
+    `${path.join(projectDir, 'package.json')}`,
+    { assert: { type: 'json' } }
+  );
+  projectPackageJson.name = projectName;
+  fs.writeFileSync(
+    path.join(projectDir, 'package.json'),
+    JSON.stringify(projectPackageJson, null, 2)
+  );
+}
+
 const projectName = process.argv[2];
+const questions = [
+  {
+    type: 'list',
+    name: 'type',
+    message: 'Do you need a server ?',
+    choices: ['Yes', 'No'],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+];
 
-// Create a project directory with the project name.
-const currentDir = process.cwd();
-const projectDir = path.resolve(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+const answers = await inquirer
+  .prompt(questions)
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log(chalk.red('Error'))
+    } else {
+      console.log(chalk.red('Error'))
+    }
+  });
 
-// A common approach to building a starter template is to
-// create a `template` folder which will house the template
-// and the files we want to create.
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const templateDir = path.resolve(__dirname, 'templates');
-fs.cpSync(path.resolve(templateDir, 'root'), projectDir, { recursive: true });
-fs.cpSync(path.resolve(templateDir, 'server'), path.resolve(projectDir, 'server'), { recursive: true });
-
-// It is good practice to have dotfiles stored in the
-// template without the dot (so they do not get picked
-// up by the starter template repository). We can rename
-// the dotfiles after we have copied them over to the
-// new project directory.
-fs.renameSync(
-  path.join(projectDir, 'gitignore'),
-  path.join(projectDir, '.gitignore')
-);
-fs.renameSync(
-  path.join(projectDir, 'github'),
-  path.join(projectDir, '.github')
-);
-fs.renameSync(
-  path.join(projectDir, 'dockerignore'),
-  path.join(projectDir, '.dockerignore')
-);
-
-// const questions = [
-//   {
-//     type: 'list',
-//     name: 'mongo',
-//     message: 'Do you need mongodb?',
-//     choices: ['Yes', 'No'],
-//     filter(val) {
-//       return val.toLowerCase();
-//     },
-//   },
-// ];
-
-// const answers = await inquirer
-//   .prompt(questions)
-//   .catch((error) => {
-//     if (error.isTtyError) {
-//       console.log(chalk.red('Error'))
-//     } else {
-//       console.log(chalk.red('Error'))
-//     }
-//   });
-
-// Update the project's package.json with the new project name
-const {default: projectPackageJson} = await import(`${path.join(projectDir, 'package.json')}`, { assert: { type: 'json' } });
-projectPackageJson.name = projectName;
-
-fs.writeFileSync(
-  path.join(projectDir, 'package.json'),
-  JSON.stringify(projectPackageJson, null, 2)
-);
-
-// // Run `npm install` in the project directory to install
-// // the dependencies. We are using a third-party library
-// // called `cross-spawn` for cross-platform support.
-// // (Node has issues spawning child processes in Windows).
-process.chdir(projectName)
-// Install client dependancies
-spawn.sync('npm', ['create', 'vite@latest', `client`, '--', '--template', 'react'], { stdio: 'inherit' });
-spawn.sync('npm', ['i', '--workspace=client', '@dataesr/react-dsfr', '@tanstack/react-query', '@tanstack/react-query-devtools', 'classnames', 'prop-types', 'react-router-dom'], { stdio: 'inherit' });
-spawn.sync('npm', ['i', '--workspace=client', '-D', 'sass'], { stdio: 'inherit' });
-spawn.sync('npx', ['--workspace=client', 'install-peerdeps', '-D', 'eslint-config-airbnb'], { stdio: 'inherit' });
-
-// Install client dependancies
-spawn.sync('npm', ['i', '--workspace=server', 'express', 'dotenv', 'cors'], { stdio: 'inherit' });
-spawn.sync('npm', ['i', '--workspace=server', '-D', 'nodemon'], { stdio: 'inherit' });
-spawn.sync('npx', ['--workspace=server', 'install-peerdeps', '-D', 'eslint-config-airbnb-base'], { stdio: 'inherit' });
-
-fs.rmSync(path.resolve(projectDir, 'client', 'src'), { recursive: true })
-fs.rmSync(path.resolve(projectDir, 'client', 'public'), { recursive: true })
-fs.mkdirSync(path.resolve(projectDir, 'client', 'public'));
-fs.mkdirSync(path.resolve(projectDir, 'client', 'src'));
-fs.cpSync(path.resolve(templateDir, 'client', 'src'), path.resolve(projectDir, 'client', 'src'), { recursive: true });
-fs.cpSync(path.resolve(templateDir, 'client', 'root'), path.resolve(projectDir, 'client'), { recursive: true });
-fs.cpSync(path.resolve(projectDir, 'node_modules', '@gouvfr', 'dsfr', 'dist', 'fonts'), path.resolve(projectDir, 'client', 'public', 'fonts'), { recursive: true });
-fs.cpSync(path.resolve(projectDir, 'node_modules', '@gouvfr', 'dsfr', 'dist', 'icons'), path.resolve(projectDir, 'client', 'public', 'icons'), { recursive: true });
-fs.cpSync(path.resolve(projectDir, 'node_modules', '@gouvfr', 'dsfr', 'dist', 'favicon'), path.resolve(projectDir, 'client', 'public'), { recursive: true });
-
-console.log('Success! Your new project is ready.');
-console.log(`Created ${projectName} at ${projectDir}`);
+if (answers.type === "no") {
+  await scaffoldClient(projectName);
+  console.log("");
+  console.log(chalk.yellow.bold(`React project scaffolded in '/${projectName}'`));
+  console.log('-------------------------');
+  console.log(` To get started, run:`);
+  console.log(chalk.italic(`  > cd ${projectName}`));
+  console.log(chalk.italic(`  > npm install`));
+  console.log(chalk.italic(`  > npm run dev`));
+  console.log('-------------------------');
+  console.log("");
+  console.log("");
+}
+if (answers.type === "yes") {
+  await scaffoldServer(projectName);
+  console.log("");
+  console.log(chalk.yellow.bold(`Fullstack project scaffolded in '/${projectName}'`));
+  console.log('-------------------------');
+  console.log(` To get started, run:`);
+  console.log(chalk.italic(`  > cd ${projectName}`));
+  console.log(chalk.italic(`  > npm install`));
+  console.log(chalk.italic(`  > npm run dev`));
+  console.log('-------------------------');
+  console.log("");
+  console.log("");
+}
---
+++ templates/fullstack/README.md
@@ -0,0 +1,10 @@
+# DOADIFY
+
+## Development
+
+Doadify is fully functional in development. 
+
+## Build for production
+
+The react client app is served by the node server in production.
+Vite build creates a build in `/dist` folder. This folder has to be moved to the `/server` folder.
